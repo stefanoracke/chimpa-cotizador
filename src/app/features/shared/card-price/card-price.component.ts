7efb68044db14f1d6f3ba2893a6b12b2968b0da9
Stefano Racke <stefano@gloria.digital>
2022-10-26 21:29:26 +0200

    card-price changes

--- src/app/features/shared/card-price/card-price.component.ts
+++ src/app/features/shared/card-price/card-price.component.ts
@@ -9,6 +9,24 @@ export class CardPriceComponent implements OnInit {
 
   constructor() { }
 
+  finaciacionobject=[
+    {
+      name:'12 Cuotas sin interés',
+      description: 'Se abona al inicio del proyecto <br> Plan Ahora 12 con tarjetas de banco Visa o MasterCard',
+      cuotas: 12 
+    },
+    {
+      name:'20% Off',
+      description: 'Contado (Efectivo o Transferencia) <br> Pago total al comenzar el proyecto',
+      cuotas: 0 
+    },
+    {
+      name:'Financiación Propia',
+      description: 'Cuotas sin interés <br> Contado (Efectivo o Transferencia)',
+      cuotas: 5 
+    }
+
+  ]
   ngOnInit(): void {
   }
 
