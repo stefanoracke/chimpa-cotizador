591f26961ef21a6d4eb548ac06bca318a7f76060
Stefano Racke <stefano@gloria.digital>
2022-10-31 20:21:31 +0100

    changes monday 4-21

--- src/app/features/views/landing/landing.module.ts
+++ src/app/features/views/landing/landing.module.ts
@@ -3,19 +3,28 @@ import { CommonModule } from '@angular/common';
 import { Section1Component } from './section1/section1.component';
 import { CardPriceComponent } from '../../shared/card-price/card-price.component';
 import { SharedModule } from '../../shared/shared.module';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
 
 
 
 @NgModule({
   declarations: [
-    Section1Component
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    Section1Component
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component
   ]
 })
 export class LandingModule { }
