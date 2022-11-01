17094bd8ec550156acfd28dcb2c3c845bbe6ed25
Stefano Racke <stefano@gloria.digital>
2022-11-01 20:05:58 +0100

    section-added

--- src/app/features/views/landing/landing.module.ts
+++ src/app/features/views/landing/landing.module.ts
@@ -6,6 +6,9 @@ import { SharedModule } from '../../shared/shared.module';
 import { Section2Component } from './section2/section2.component';
 import { Section3Component } from './section3/section3.component';
 import { Section4Component } from './section4/section4.component';
+import { Section5Component } from './section5/section5.component';
+import { Section6Component } from './section6/section6.component';
+import { IncluidosComponent } from './incluidos/incluidos.component';
 
 
 
@@ -14,7 +17,10 @@ import { Section4Component } from './section4/section4.component';
     Section1Component,
     Section2Component,
     Section3Component,
-    Section4Component
+    Section4Component,
+    Section5Component,
+    Section6Component,
+    IncluidosComponent
   ],
   imports: [
     CommonModule,
