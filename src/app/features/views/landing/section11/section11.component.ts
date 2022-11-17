37235ade88fcb4fa62daf21fc0476f8043b7a88c
Stefano Racke <stefano@gloria.digital>
2022-11-17 19:14:02 +0100

    Section 12 and footer

--- src/app/features/views/landing/section11/section11.component.ts
+++ src/app/features/views/landing/section11/section11.component.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section11.component.scss']
 })
 export class Section11Component implements OnInit {
-
+  n=  0;
+  mostrarContenido = true
   constructor() { }
   title='Testimonios'
   ngOnInit(): void {
+    setTimeout(()=>{
+      this.mostrarContenido=false
+    },5500)
+    setInterval(
+      ()=>{
+        
+        this.mostrarContenido = true
+        if(this.n == this.testimonios.length-1){
+          this.n=0
+          }
+        else{this.n++
+        }
+
+        setTimeout(()=>{
+          this.mostrarContenido=false
+        },5500)
+      },6000
+    )
   }
+
+  testimonios = [
+    {
+      name: 'Rodrigo Rodriguez',
+      testimonio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut assumenda magnam, in veritatis necessitatibus facilis quia repudiandae. Repellendus, voluptatibus animi vel, quaerat accusantium sunt voluptate fugit, dolorem alias nobis quibusdam.',
+      empresa: 'Cinergia'
+    },
+    {
+      name: 'Pablo Rodriguez',
+      testimonio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut assumenda magnam, in veritatis necessitatibus facilis quia repudiandae. Repellendus, voluptatibus animi vel, quaerat accusantium sunt voluptate fugit, dolorem alias nobis quibusdam.',
+      empresa: 'Costa Esmeralda'
+    },
+    {
+      name: 'Pesadas de España',
+      testimonio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut assumenda magnam, in veritatis necessitatibus facilis quia repudiandae. Repellendus, voluptatibus animi vel, quaerat accusantium sunt voluptate fugit, dolorem alias nobis quibusdam.',
+      empresa: 'Popatik'
+    },
+    
+  ]
+
 
 }
