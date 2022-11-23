264692441a51b8e34d9bce4aced3452e1c7737b4
Stefano Racke <stefano@gloria.digital>
2022-11-23 19:33:44 +0100

    section8 and firehost

--- src/app/features/views/landing/section8/section8.component.ts
+++ src/app/features/views/landing/section8/section8.component.ts
@@ -9,6 +9,39 @@ export class Section8Component implements OnInit {
 
   constructor() { }
   title= "Metodología"
+  metodology = [
+    {
+      name:'BRIEF',
+      image: 'assets/images/metodologia/image_0.png'
+    },
+    {
+      name:'UX',
+      image: 'assets/images/metodologia/image_1.svg',
+      number: 'assets/images/metodologia/number_1.svg'
+    },
+    {
+      name:'UI',
+      image: 'assets/images/metodologia/image_2.png',
+      number: 'assets/images/metodologia/number_2.svg'
+    },
+    {
+      name:'PROGRAMACION',
+      image: 'assets/images/metodologia/image_3.png',
+      number: 'assets/images/metodologia/number_3.svg'
+    },
+    {
+      name:'TEST',
+      image: 'assets/images/metodologia/image_4.png',
+      number: 'assets/images/metodologia/number_4.svg'
+    },
+    {
+      name:'FINAL',
+      image: 'assets/images/metodologia/image_5.png',
+      number: 'assets/images/metodologia/number_5.svg'
+    },
+    
+  ]
+
   ngOnInit(): void {
   }
 
