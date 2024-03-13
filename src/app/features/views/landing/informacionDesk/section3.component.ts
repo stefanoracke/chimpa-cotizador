a3603795b718ffeb9bd2dec4015ccfbf3b51d613
stefanoracke <stefanoracke@gmail.com>
2024-03-13 16:08:27 -0300

    cambiando si no hay funcionalidades o caract

--- src/app/features/views/landing/informacionDesk/section3.component.ts
+++ src/app/features/views/landing/informacionDesk/section3.component.ts
@@ -17,24 +17,7 @@ export class Section3Component implements OnInit, OnDestroy {
   n = 0
   subs$!:Subscription
   title:any
-  funcionalidades:Array<any> =[
-    {
-      title: 'Sitio web con 4 páginas y 5 secciones cada una.',
-      list: []
-    },
-    {
-      title: 'Funcionalidades especificas.',
-      list: [
-        'Página principal - home', 'Dos páginas secundarias', 'Página de contacto: formulario + botón de whatsapp'
-      ]
-    },
-    {
-      title: 'Distintas herramientas para mejorar tu web.',
-      list: [
-        'Bootstrap', 'Dos páginas secundarias','Bootstrap', 'Dos páginas secundarias','Bootstrap', 'Dos páginas secundarias', 'Página de contacto: formulario + botón de whatsapp'
-      ]
-    }
-  ]
+  funcionalidades!:Array<any>
 
 
 
@@ -42,16 +25,12 @@ export class Section3Component implements OnInit, OnDestroy {
     this.subs$ = this.store.select(selectAllPropuesta).subscribe(res=>{
       this.title = res.title_option
       this.funcionalidades = [{
-        title: 'Sitio web con 4 páginas y 5 secciones cada una.',
         list: res.features_type[2].content.map((contenido:any)=>contenido.description)
       },
       {
-        title: 'Sitio web con 4 páginas y 5 secciones cada una.',
         list: res.features_type[3].content.map((contenido:any)=>contenido.description)
       },
-      
       {
-        title: 'Sitio web con 4 páginas y 5 secciones cada una.',
         list: res.features_type[0].content.map((contenido:any)=>contenido.description)
       },
     ]
